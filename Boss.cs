897c8af8d04e267c7feed5fd71b94eed5ffca52d
Natalie Weizenbaum <nex342@gmail.com>
2024-06-30 23:23:20 -0700

    Factor out boss data into a bigger class that we can use to generate HTML.

---
+++ Boss.cs
@@ -0,0 +1,31 @@
+﻿public enum DamageType
+{
+    Standard, Slash, Strike, Pierce, Magic, Fire, Lightning, Holy
+}
+
+public class Boss
+{
+    public Boss(int id, string name, int gameAreaID)
+    {
+        ID = id;
+        Name = name;
+        GameAreaID = gameAreaID;
+        DamageTypes = [];
+        TypeDefense = [];
+        HP = [];
+        Defense = [];
+        Runes = [];
+        Resistance = [];
+    }
+
+    public int ID { get; init; }
+    public string Name { get; init; }
+    public int GameAreaID { get; init; }
+    public int Stance { get; set; }
+    public SortedSet<DamageType> DamageTypes { get; }
+    public Dictionary<string, int> TypeDefense { get; }
+    public List<int> HP { get; }
+    public List<int> Defense { get; }
+    public List<int> Runes { get; }
+    public Dictionary<string, List<List<int>>> Resistance { get; }
+}
--- Program.cs
+++ Program.cs
@@ -64,16 +64,13 @@ foreach (var attack in attacks.Rows)
 
 foreach (var boss in bosses)
 {
-    Console.WriteLine();
-    Console.WriteLine($"# {boss.Name}");
     var bossParams = npcs[boss.ID];
 
-    Console.WriteLine($"Stance: {bossParams["superArmorDurability"].Value}");
+    boss.Stance = (int)Math.Round((float)bossParams["superArmorDurability"].Value);
 
     var behavior = bossParams["behaviorVariationId"];
     if (attacksForEnemy.TryGetValue((int)behavior.Value / 10, out var bossAttacks))
     {
-        var types = new SortedSet<DamageType>();
         foreach (var attack in bossAttacks)
         {
             if ((ushort)attack["atkPhys"].Value > 0)
@@ -81,33 +78,39 @@ foreach (var boss in bosses)
                 switch ((byte)attack["atkAttribute"].Value)
                 {
                     case 0:
-                        types.Add(DamageType.Slash);
+                        boss.DamageTypes.Add(DamageType.Slash);
                         break;
                     case 1:
-                        types.Add(DamageType.Strike);
+                        boss.DamageTypes.Add(DamageType.Strike);
                         break;
                     case 2:
-                        types.Add(DamageType.Pierce);
+                        boss.DamageTypes.Add(DamageType.Pierce);
                         break;
                     default:
-                        types.Add(DamageType.Standard);
+                        boss.DamageTypes.Add(DamageType.Standard);
                         break;
                 }
             }
 
-            if ((ushort)attack["atkMag"].Value > 0) types.Add(DamageType.Magic);
-            if ((ushort)attack["atkFire"].Value > 0) types.Add(DamageType.Fire);
+            if ((ushort)attack["atkMag"].Value > 0)
+            {
+                boss.DamageTypes.Add(DamageType.Magic);
+            }
+            if ((ushort)attack["atkFire"].Value > 0)
+            {
+                boss.DamageTypes.Add(DamageType.Fire);
+            }
             if ((ushort)attack["atkThun"].Value > 0)
             {
-                types.Add(DamageType.Lightning);
+                boss.DamageTypes.Add(DamageType.Lightning);
             }
-            if ((ushort)attack["atkDark"].Value > 0) types.Add(DamageType.Holy);
+            if ((ushort)attack["atkDark"].Value > 0)
+            {
+                boss.DamageTypes.Add(DamageType.Holy);
+            }
         }
-
-        Console.WriteLine($"Damage Types: {String.Join(", ", types)}");
     }
 
-    Console.WriteLine("## Defenses");
     List<(string, string)> defenseParams = [
         ("Standard", "neutralDamageCutRate"),
         ("Slash", "slashDamageCutRate"),
@@ -121,50 +124,50 @@ foreach (var boss in bosses)
     foreach (var (name, param) in defenseParams)
     {
         var value = 1 - (float)bossParams[param].Value;
-        Console.WriteLine($"{name}: {Math.Round(100 * value)}");
+        boss.TypeDefense[name] = (int)Math.Round(100 * value);
     }
 
     var ngScaling = spEffects[(int)bossParams["spEffectID3"].Value];
     var ngpScaling = spEffects[(int)bossParams["GameClearSpEffectID"].Value];
 
-    Console.WriteLine("##");
-
     var baseHP = (uint)bossParams["hp"].Value;
-    var ngHp = baseHP * (float)ngScaling["maxHpRate"].Value;
-    var ngpHP = ngHp * (float)ngpScaling["maxHpRate"].Value;
-    List<float> hps = [ngHp, ngpHP];
+    var ngHP = baseHP * (float)ngScaling["maxHpRate"].Value;
+    var ngpHP = ngHP * (float)ngpScaling["maxHpRate"].Value;
+    boss.HP.Add((int)Math.Floor(ngHP));
+    boss.HP.Add((int)Math.Floor(ngpHP));
     for (var i = 2; i < 8; i++)
     {
-        hps.Add(ngpHP * (float)clearCountParams[i]["MaxHpRate"].Value);
+        boss.HP.Add((int)Math.Floor(ngpHP * (float)clearCountParams[i]["MaxHpRate"].Value));
     }
-    Console.WriteLine($"HP: {String.Join("/", hps.Select((def) => Math.Floor(def)))}");
 
     // Base defense for all stats seems to be 100, and defense scaling seems to be consistent across
     // all damage types. Defense is also not multiplicative between NG and NG+ according to Phil.
     var ngDefense = 100 * (float)ngScaling["physicsDiffenceRate"].Value;
     var ngpDefense = 100 * (float)ngpScaling["physicsDiffenceRate"].Value;
-    List<float> defences = [ngDefense, ngpDefense];
+    boss.Defense.Add((int)Math.Floor(ngDefense));
+    boss.Defense.Add((int)Math.Floor(ngpDefense));
     for (var i = 2; i < 8; i++)
     {
-        defences.Add(ngpDefense * (float)clearCountParams[i]["PhysicsDefenseRate"].Value);
+        boss.Defense.Add(
+            (int)Math.Floor(ngpDefense * (float)clearCountParams[i]["PhysicsDefenseRate"].Value)
+        );
     }
-    Console.WriteLine($"Defense: {String.Join("/", defences.Select((hp) => Math.Floor(hp)))}");
 
     var ngRunes = (uint)gameAreaParam[boss.GameAreaID]["bonusSoul_single"].Value;
     var ngpRunes = ngRunes * (float)ngpScaling["haveSoulRate"].Value;
-    List<float> runes = [ngRunes, ngpRunes];
+    boss.Runes.Add((int)ngRunes);
+    boss.Runes.Add((int)Math.Round(ngpRunes));
     for (var i = 2; i < 8; i++)
     {
-        runes.Add(ngpRunes * (float)clearCountParams[i]["SoulRate"].Value);
+        boss.Runes.Add((int)Math.Round(ngpRunes * (float)clearCountParams[i]["SoulRate"].Value));
     }
-    Console.WriteLine($"Runes: {String.Join("/", runes.Select((runes) => Math.Floor(runes)))}");
-
-    Console.WriteLine("## Resistances");
 
     List<List<int>> resistances(
         String baseCell, String correctCell, String rateCell, String clearCountCell
     ) {
         var baseValue = (ushort)bossParams[baseCell].Value;
+        if (baseValue == 999) return [];
+
         var correct = resistCorrectParam[(int)bossParams[correctCell].Value];
         var ngProc1 = baseValue * (float)ngScaling[rateCell].Value;
         var ngpProc1 = ngProc1 * (float)ngpScaling[rateCell].Value;
@@ -188,39 +191,40 @@ foreach (var boss in bosses)
         ).ToList();
     }
 
-    writeResistances(
-        "Poison",
-        resistances(
-            "resist_poison",
-            "resistCorrectId_poison",
-            "registPoizonChangeRate",
-            "PoisionResistRate"
-        )
+    boss.Resistance["Poison"] = resistances(
+        "resist_poison",
+        "resistCorrectId_poison",
+        "registPoizonChangeRate",
+        "PoisionResistRate"
     );
-}
-
-void writeResistances(string name, List<List<int>> resistances)
-{
-    Console.WriteLine($"{name}: {
-        String.Join(';', resistances.Select((procs) => String.Join('/', procs)))
-    }");
-}
-
-enum DamageType
-{
-    Standard, Slash, Strike, Pierce, Magic, Fire, Lightning, Holy
-}
-
-readonly struct Boss
-{
-    public Boss(int id, string name, int gameAreaID)
-    {
-        ID = id;
-        Name = name;
-        GameAreaID = gameAreaID;
-    }
-
-    public int ID { get; init; }
-    public string Name { get; init; }
-    public int GameAreaID { get; init; }
+    boss.Resistance["Scarlet Rot"] = resistances(
+        "resist_desease",
+        "resistCorrectId_disease",
+        "registDiseaseChangeRate",
+        "DiseaseResistRate"
+    );
+    boss.Resistance["Hemorrhage"] = resistances(
+        "resist_blood",
+        "resistCorrectId_blood",
+        "registBloodChangeRate",
+        "BloodResistRate"
+    );
+    boss.Resistance["Frostbite"] = resistances(
+        "resist_freeze",
+        "resistCorrectId_freeze",
+        "registFreezeChangeRate",
+        "FreezeResistRate"
+    );
+    boss.Resistance["Sleep"] = resistances(
+        "resist_sleep",
+        "resistCorrectId_sleep",
+        "registSleepChangeRate",
+        "SleepResistRate"
+    );
+    boss.Resistance["Madness"] = resistances(
+        "resist_madness",
+        "resistCorrectId_madness",
+        "registMadnessChangeRate",
+        "MadnessResistRate"
+    );
 }
